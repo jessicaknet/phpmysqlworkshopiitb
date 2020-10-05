b9ababcc25318a69fa7ef9a1065a0832a8d3fd0e
unknown <netalkarjeka19it@student.mes.ac.in>
2020-10-05 20:58:13 +0530

    First commit

---
+++ day1/q1_2_vowel.php
@@ -0,0 +1,30 @@
+<?php
+
+$letter="i";
+
+switch($letter)
+{
+case "a":
+echo "the ".$letter." is a vowel";
+break;
+
+case "e":
+echo "the ".$letter." is a vowel";
+break;
+
+case "i":
+echo "the ".$letter." is a vowel";
+break;
+
+case "o":
+echo "the ".$letter." is a vowel";
+break;
+
+case "u":
+echo "the ".$letter." is a vowel";
+break;
+
+default: 
+echo "the ".$letter." is a consonant";
+}
+?>
